ef10296c29763335b71aad4caac5faafeb9cafe1
andstor <andr3.storhaug@gmail.com>
2022-03-31 01:11:40 +0200

    Add backoff

--- script/contracts_downloader.py
+++ script/contracts_downloader.py
@@ -3,12 +3,15 @@ import os
 from turtle import st
 from datasets import DownloadManager
 from etherscan.contracts import Contract
+from etherscan.client import EmptyResponse, BadRequest, ConnectionRefused
+import backoff
 import json
 import sys
 import argparse
 from pathlib import Path
 import math
 from tqdm import tqdm
+
 
 class ContractsDownloadManager:
     def __init__(self, token, addresses="all_contracts.csv", output="data", shard=1, index=0, skip=0, position=0, **kwargs):
@@ -60,7 +63,8 @@ class ContractsDownloadManager:
 
         with open(self.addresses_path) as fp:
             line = fp.readline()
-            pbar = tqdm(total=batch, position=self.position, desc="Shard " + str(self.index+1) + "/" + str(self.shard))
+            pbar = tqdm(total=batch, position=self.position,
+                        desc="Shard " + str(self.index+1) + "/" + str(self.shard))
             while line:
                 if (count >= end):
                     break
@@ -76,7 +80,7 @@ class ContractsDownloadManager:
                     line = fp.readline()
                     continue
                 contract_path = Path(self.output_dir, address_path + '.json')
-                pbar.update(1) # update progress bar
+                pbar.update(1)  # update progress bar
                 meta = {}
                 meta["token"] = self.token
                 meta["index"] = str(count) + "/" + str(adress_count)
@@ -87,11 +91,8 @@ class ContractsDownloadManager:
                 count_effective += 1
                 meta["empty"] = str(round(empty*100/count_effective, 2)) + "%"
                 pbar.set_postfix(meta)
-                #print(count, '/', adress_count, round(count_effective * 100 / batch, 2),
-                #    '%', 'empty', empty, round(empty*100/count_effective, 2), '%')
                 try:
-                    api = Contract(address=address_path, api_key=self.token)
-                    sourcecode = api.get_sourcecode()
+                    sourcecode = self.download_contract(address=address_path)
                     if len(sourcecode[0]['SourceCode']) == 0:
                         empty += 1
                     with open(contract_path, 'w') as fd:
@@ -103,8 +104,15 @@ class ContractsDownloadManager:
                         json.dump(not_valid, fd)
                     print(identifier)
                 line = fp.readline()
+    
 
-
+    @backoff.on_exception(backoff.expo,
+                          (EmptyResponse, BadRequest, ConnectionRefused),
+                          max_tries=8)
+    def download_contract(self, address):
+        api = Contract(address=address, api_key=self.token)
+        sourcecode = api.get_sourcecode()
+        return sourcecode
 
 
 if __name__ == "__main__":
@@ -134,5 +142,6 @@ if __name__ == "__main__":
     index = args.index
     skip = args.skip
 
-    cdm = ContractsDownloadManager(token, addresses_path, output_dir, shard, index, skip)
+    cdm = ContractsDownloadManager(
+        token, addresses_path, output_dir, shard, index, skip)
     cdm.download()
