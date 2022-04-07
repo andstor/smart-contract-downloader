33d0977fd1eb9464baf4f06e726bca1470276085
andstor <andr3.storhaug@gmail.com>
2022-04-07 05:16:43 +0200

    Add progress bar

--- script/data_processor.py
+++ script/data_processor.py
@@ -3,6 +3,8 @@ import pandas as pd
 from pathlib import Path
 from contract import Contract
 from datasets import Dataset
+from tqdm import tqdm
+
 
 class DataProcessor():
 
@@ -10,22 +12,24 @@ class DataProcessor():
 
         self.dir_path = dir_path
         self.chunk_size = chunk_size
-
+        
         self.data = self._read_parquet(self.dir_path)
         self._buffer = None
         self._unique_file_names = pd.Series(dtype=str)
 
-    @classmethod
-    def _read_parquet(cls, dir_path):
+        files_count = sum(1 for f in Path(self.dir_path).glob("*.parquet"))
+        self.pbar = tqdm(total=files_count)
+
+    def _read_parquet(self, dir_path):
         """
         TODO: Write docstring
         """
+        
         index = 0
         while True:
-            file_path = Path(dir_path, "part." + str(index) + ".parquet").resolve()
+            file_path = Path(dir_path, "part." +
+                             str(index) + ".parquet").resolve()
             if file_path.exists():
-
-                print("Reading file: " + str(index))
                 shard = pd.read_parquet(file_path)
                 index += 1
                 yield shard
@@ -51,32 +55,32 @@ class DataProcessor():
         """
         file_names = df.apply(lambda row: row.file_path.split("/")[-1], axis=1)
         df["file_name"] = file_names
-        
 
         dupes = df["file_name"].isin(self._unique_file_names)
         dupes[df['file_name'] == ''] = False
         df = df[~dupes]
         # Keeping first since all_contracts is sorted on most transactions first.
-        df = df.drop_duplicates(subset=['file_name'], keep='first') 
-        
-        self._unique_file_names = pd.concat([self._unique_file_names, file_names], axis=0, ignore_index=True).drop_duplicates()
+        df = df.drop_duplicates(subset=['file_name'], keep='first')
+
+        self._unique_file_names = pd.concat(
+            [self._unique_file_names, file_names], axis=0, ignore_index=True).drop_duplicates()
         return df
-        
 
     def plain_text(self) -> Iterable[Dataset]:
         """
         This function takes a list of strings and returns a list of strings
         with duplicates removed.
         """
-
+        self.pbar.set_description("Dataset \"plain_text\"")
         for batch in self.data:
-            df = self._explode_files(batch) # Potentially very large batch
+            self.pbar.update(1)
+            df = self._explode_files(batch)  # Potentially very large batch
             df = self._uniqify(df)
             df = df.rename(columns={'source_code': 'text'})
             df = df[['text', 'language']].reset_index()
             chunk = self.chunk(df)
             if chunk is not None:
-                yield self.split_train_test(chunk)
+                yield chunk
             else:
                 continue
         else:
@@ -86,17 +90,19 @@ class DataProcessor():
                 self._buffer = self._buffer.iloc[self.chunk_size:]
                 if self._buffer.shape[0] == 0:
                     self._buffer = None
-                yield self.split_train_test(chunk)
-            
+                yield chunk
+
     def all(self) -> Iterable[Dataset]:
         """
         This function takes a list of strings and returns a list of strings
         with duplicates removed.
         """
+        self.pbar.set_description("Dataset \"all\"")
         for batch in self.data:
+            self.pbar.update(1)
             chunk = self.chunk(batch)
             if chunk is not None:
-                yield self.split_train_test(chunk)
+                yield chunk
             else:
                 continue
         else:
@@ -106,16 +112,16 @@ class DataProcessor():
                 self._buffer = self._buffer.iloc[self.chunk_size:]
                 if self._buffer.shape[0] == 0:
                     self._buffer = None
-                yield self.split_train_test(chunk)
+                yield chunk
 
     def chunk(self, df: pd.DataFrame) -> pd.DataFrame:
         """
         This function rebalances the dataset.
         """
-        
+
         if self._buffer is not None:
             df = pd.concat([self._buffer, df], axis=0, ignore_index=True)
-        
+
         if df.shape[0] < self.chunk_size:
             # If the batch is smaller than the chunk size,
             # we buffer it and add it to the next batch.
@@ -128,24 +134,17 @@ class DataProcessor():
             self._buffer = df.iloc[self.chunk_size:]
             return chunk
 
-    def split_train_test(self, df, test_size=0.05):
-        """
-        This function splits the dataset into train and test data.
-        """
-        contracts_ds = Dataset.from_pandas(df)
-        datasets = contracts_ds.train_test_split(test_size=test_size, seed=1, keep_in_memory=True)
-
-        return datasets
-
 
 if __name__ == '__main__':
     import argparse
+    import os
+
     parser = argparse.ArgumentParser(
         description='Process dataset.')
 
     parser.add_argument('-s', '--source', metavar='source', type=str, required=False,
                         default="parquet", help='Path to directory with files to process.')
-    parser.add_argument('-o', '--output-dir', metavar='output-dir', type=str, required=False,
+    parser.add_argument('-o', '--output-dir', metavar='output_dir', type=str, required=False,
                         default="data", help='The directory where the output should be stored.')
     parser.add_argument('--chunk-size', metavar='chunk-size', type=int, required=False,
                         default=50000, help='The number of contracts to store in each data file.')
@@ -155,21 +154,32 @@ if __name__ == '__main__':
                         default=True, help='Wheter to clean existing files in output dir.')
     args = parser.parse_args()
 
-    if args.clean:
-        print("Cleaning output dir")
-        for file in Path(args.output_dir).glob("*.parquet"):
-            file.unlink()
+    for dataset_name in args.datasets:
 
-    for dataset in args.datasets:
-        if dataset == "all":
+        if dataset_name == "all":
+            if not os.path.exists(args.output_dir):
+                os.makedirs(args.output_dir)
+            if args.clean:
+                for file in Path(args.output_dir, dataset_name).glob("*.parquet"):
+                    file.unlink()
+
             dp = DataProcessor(args.source, args.chunk_size).all()
-            for index, datasets in enumerate(dp):
-                datasets['train'].to_parquet(Path(args.output_dir, dataset, "part." + str(index) + ".parquet"))
-                datasets['test'].to_parquet(Path(args.output_dir, dataset, "part." + str(index) + ".parquet"))
-        elif dataset == "plain_text":
+            for index, dataset in enumerate(dp):
+                contracts_ds = Dataset.from_pandas(dataset)
+                path = os.path.join(args.output_dir, dataset_name, "part." + str(index) + ".parquet")
+                contracts_ds.to_parquet(path)
+
+        elif dataset_name == "plain_text":
+            if not os.path.exists(args.output_dir):
+                os.makedirs(args.output_dir)
+            if args.clean:
+                for file in Path(args.output_dir, dataset_name).glob("*.parquet"):
+                    file.unlink()
+
             dp = DataProcessor(args.source, args.chunk_size).plain_text()
-            for index, datasets in enumerate(dp):
-                datasets['train'].to_parquet(Path(args.output_dir, dataset, "part." + str(index) + ".parquet"))
-                datasets['test'].to_parquet(Path(args.output_dir, dataset, "part." + str(index) + ".parquet"))
+            for index, dataset in enumerate(dp):
+                contracts_ds = Dataset.from_pandas(dataset)
+                path = os.path.join(args.output_dir, dataset_name, "part." + str(index) + ".parquet")
+                contracts_ds.to_parquet(path)
         else:
             raise ValueError("Unknown dataset: " + dataset)
